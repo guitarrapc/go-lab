9d6473db5589a3ac891c0cbb560ce8354348ee9b
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-02-01 12:43:39 +0900

    fix: dirwalk

--- ioLab/directory/dirwalk/main.go
+++ ioLab/directory/dirwalk/main.go
@@ -8,32 +8,37 @@ import (
 )
 
 func main() {
-	pattern := regexp.MustCompile(filepath.ToSlash(`^D:\GitHub\guitarrapc\MixedContentChecker\csharp\src\.*\bin\.+\netcoreapp2.2$`))
-	dirs := dirwalkDirectory(`D:\GitHub\guitarrapc\MixedContentChecker`, true)
+	pattern := regexp.MustCompile(`^D:/GitHub/guitarrapc/MixedContentChecker/csharp/src/.*/bin/.+/netcoreapp2.2$`)
+	dirs, err := dirwalk(`D:\GitHub\guitarrapc\MixedContentChecker`, true)
+	if err != nil {
+		fmt.Println(err)
+	}
 	for _, dir := range dirs {
 		fmt.Println(pattern.MatchString(dir), dir)
 	}
 }
 
-func dirwalkDirectory(dir string, toSlash bool) []string {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		panic(err)
+func dirwalk(path string, toSlash bool) (fullPaths []string, err error) {
+	fullPaths = nil
+	files, _err := ioutil.ReadDir(path)
+	if _err != nil {
+		err = _err
+		return
 	}
 
-	var paths []string
 	for _, file := range files {
 		if file.IsDir() {
 			if toSlash {
-				paths = append(paths, filepath.ToSlash(filepath.Join(dir, file.Name())))
+				fullPaths = append(fullPaths, filepath.ToSlash(filepath.Join(path, file.Name())))
 			} else {
-				paths = append(paths, filepath.Join(dir, file.Name()))
+				fullPaths = append(fullPaths, filepath.Join(path, file.Name()))
 			}
 
-			paths = append(paths, dirwalkDirectory(filepath.Join(dir, file.Name()), toSlash)...)
+			walkPaths, _err := dirwalk(filepath.Join(path, file.Name()), toSlash)
+			fullPaths = append(fullPaths, walkPaths...)
+			err = _err
 			continue
 		}
 	}
-
-	return paths
+	return
 }
