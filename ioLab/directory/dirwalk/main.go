d072c262af75e67e62b6cf76208353feb05f929d
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-02-07 19:36:42 +0900

    fix: basePath was delay for 1 loop timing

--- ioLab/directory/dirwalk/main.go
+++ ioLab/directory/dirwalk/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"regexp"
 	"regexp/syntax"
@@ -14,14 +15,19 @@ import (
 
 func main() {
 	args := []string{
-		`^D:/GitHub/guitarrapc/MixedContent.*/csharp/src/.*/bin/.+/netcoreapp2.2$`,
-		`^D:/GitHub/guitarrapc/MixedContentChecker/csharp/src/.*/bin/.+/netcoreapp2.2$`,
-		`^D:/GitHub/ghoasd/`,
-		`^D:/GitHub/ghoasd世界/.*`,
-		`^D:/GitHub/ghoasd.*`,
-		`^D:/asdf\d{0}/ghoasd.*`,
-		`^D:/GitHub/ghoasd.*hogemoge/fugafuga/.*gua/`,
-		`^C:/Users/ikiru\.yoshizaki/Documents/Git/guitarrapc/Log4NetConfigurations/src/.*/bin/Debug$`,
+		// `^D:/GitHub/guitarrapc/MixedContent.*/csharp/src/.*/bin/.+/netcoreapp2.2$`,
+		// `^D:/GitHub/guitarrapc/MixedContentChecker/csharp/src/.*/bin/.+/netcoreapp2.2$`,
+		// `^D:/GitHub/ghoasd/`,
+		// `^D:/GitHub/ghoasd世界/.*`,
+		// `^D:/GitHub/ghoasd.*`,
+		// `^D:/asdf\d{0}/ghoasd.*`,
+		// `^D:/GitHub/ghoasd.*hogemoge/fugafuga/.*gua/`,
+		// `^C:/Users/ikiru\.yoshizaki/Documents/Git/guitarrapc/Log4NetConfigurations/src/.*/bin/Debug$`,
+		// `^C:/Users/guitarrapc/Downloads/watchdog/logfiles`,
+		// "^C:/Users/guitarrapc/Downloads/watchdog/logfiles",
+		// `^C:/Users/ikiru\.yoshizaki/go/src/github\.com/guitarrapc/watchdog-symlinker/logfiles.*$`,
+		// "^C:/Users/ikiru\\.yoshizaki/go/src/github\\.com/guitarrapc/watchdog-symlinker/logfiles.*$",
+		`^C:/Users/ikiru\.yoshizaki/go/src/github\.com/guitarrapc/watchdog-symlinker/logfiles.*$`,
 	}
 	for _, arg := range args {
 		basePath, err := getBasePath(arg)
@@ -33,13 +39,12 @@ func main() {
 
 		pattern := regexp.MustCompile(arg)
 		dirs, err := dirwalk(basePath, true)
+		dirs = append(dirs, basePath)
 		if err != nil {
 			fmt.Println(err)
 		}
 		for _, dir := range dirs {
-			if pattern.MatchString(dir) {
-				fmt.Println(pattern.MatchString(dir), dir)
-			}
+			fmt.Println(pattern.MatchString(dir), dir)
 		}
 	}
 }
@@ -51,29 +56,34 @@ func getBasePath(path string) (string, error) {
 		return "", err
 	}
 
-	var b strings.Builder
-	begin := false
-
 	// for _, a := range asts {
 	// 	fmt.Println(a)
 	// }
 
+	var b strings.Builder
+	start := false
+	begin := false
 	var s string
 	for _, a := range asts {
+		if a.IsStart {
+			start = true
+		}
+		if start && a.IsRune {
+			b.WriteString(a.Value)
+			begin = true
+		}
 		if begin && !a.IsRune {
 			// check path is valid and fix
 			s = b.String()
+			fmt.Println(s)
+			if isExists(s) {
+				break
+			}
 			if getLastRune(s, 1) != "/" {
 				i := strings.LastIndex(s, "/") + 1
 				s = string(s[:i])
 			}
 			break
-		}
-		if begin && a.IsRune {
-			b.WriteString(a.Value)
-		}
-		if a.IsStart {
-			begin = true
 		}
 	}
 
@@ -81,7 +91,6 @@ func getBasePath(path string) (string, error) {
 }
 
 func dirwalk(path string, toSlash bool) (fullPaths []string, err error) {
-	fullPaths = nil
 	files, _err := ioutil.ReadDir(path)
 	if _err != nil {
 		err = _err
@@ -124,4 +133,11 @@ func substring(str string, start int, length int) string {
 	}
 
 	return string(r[start : start+length])
+}
+
+func isExists(path string) bool {
+	if f, err := os.Stat(path); !os.IsNotExist(err) {
+		return f.IsDir()
+	}
+	return false
 }
