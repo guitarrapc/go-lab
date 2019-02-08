66b9475ef677bfa983752ec549e6f25d7138801f
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-02-08 12:40:53 +0900

    feat: filter event to file

--- ioLab/filesystem_watcher/notify/main.go
+++ ioLab/filesystem_watcher/notify/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"path/filepath"
+	"regexp"
 
 	"github.com/rjeczalik/notify"
 )
@@ -14,16 +16,37 @@ func main() {
 
 	// Set up a watchpoint listening on events within current working directory.
 	// Dispatch each create and remove events separately to c.
-	if err := notify.Watch("C:/Users/ikiru.yoshizaki/go/src/github.com/guitarrapc/go-lab/ioLab/filesystem_watcher/logfiles/hogemoge", c, notify.Create); err != nil {
+	// notify.Create event will dispatch via both folder and file.
+	// notify.FileNotifyChangeXxxxx to restrict event only file.
+	if err := notify.Watch("C:/Users/ikiru.yoshizaki/go/src/github.com/guitarrapc/go-lab/ioLab/filesystem_watcher/logfiles/hogemoge", c, notify.FileNotifyChangeFileName); err != nil {
 		log.Fatal(err)
 	}
 	defer notify.Stop(c)
+
+	pattern := regexp.MustCompile(".*\\.log")
 
 	// Block until an event is received.
 	for {
 		select {
 		case ei := <-c:
-			log.Println("Got event:", ei)
+			if pattern.MatchString(ei.Path()) {
+				log.Println("Got event:", ei)
+				// file event have many subevents.
+				// switch to use appropriate event.
+				// match event: https://github.com/rjeczalik/notify/issues/10#issuecomment-66179535
+				switch ei.Event() {
+				case notify.FileActionAdded:
+					log.Println("added!!!!!", filepath.Base(ei.Path()))
+				case notify.FileActionModified:
+					log.Println("modified!!!!!", filepath.Base(ei.Path()))
+				case notify.FileActionRemoved:
+					log.Println("removed!!!!!", filepath.Base(ei.Path()))
+				case notify.FileActionRenamedNewName:
+					log.Println("renamedNewName!!!!!", filepath.Base(ei.Path()))
+				case notify.FileActionRenamedOldName:
+					log.Println("renamedOldName!!!!!", filepath.Base(ei.Path()))
+				}
+			}
 		}
 	}
 }
