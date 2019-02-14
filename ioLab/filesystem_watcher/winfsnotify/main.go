9ea10f00f4b24d31046072c929c9109cdb88c0d6
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-02-14 13:33:14 +0900

    feat: added winfsnotify file event

---
+++ ioLab/filesystem_watcher/winfsnotify/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/exp/winfsnotify"
+)
+
+func main() {
+	watcher, err := winfsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	path := "C:/Users/ikiru.yoshizaki/go/src/github.com/guitarrapc/go-lab/ioLab/filesystem_watcher/winfsnotify/logs"
+	err = watcher.AddWatch(path, winfsnotify.FS_CREATE)
+	if err != nil {
+		log.Fatalf("Watcher.Watch() failed: %s", err)
+	}
+
+	go func() {
+		for err := range watcher.Error {
+			log.Fatalf("error received: %s", err)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case e := <-watcher.Event:
+				log.Printf("create event detected. %s, %s", e.Name, e.String())
+			}
+		}
+	}()
+
+	f, err := os.Create(filepath.Join(path, "hogemoge.log"))
+	f.Close()
+
+	end := make(chan string, 1)
+	<-end
+}
